7d5d1ef40d3fc8c546b4c36bcc426851605f1b69
taran <beer.taran@mail.ru>
2022-10-30 22:30:32 +0300

    Lesson005 Lena

---
+++ Lesson005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Лена")
+{
+Console.WriteLine("Ура, это же Лена!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
