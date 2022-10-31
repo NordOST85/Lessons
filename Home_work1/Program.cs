03f3f6948b6a2fe290026414818b0dfde69796f5
taran <beer.taran@mail.ru>
2022-10-31 04:46:04 +0300

    task2

---
+++ Home_work1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введи первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num1 > num2)
+{
+   Console.WriteLine("Первое число больше второго");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine("Первое число меньше второго");
+}
+Console.WriteLine();
