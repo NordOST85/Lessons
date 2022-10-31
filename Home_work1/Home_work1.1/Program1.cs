90bd27bd0abc42cb42ff07d3a4e90b2caa585806
taran <beer.taran@mail.ru>
2022-10-31 05:43:15 +0300

    home_work_need_work

---
+++ Home_work1/Home_work1.1/Program1.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введи первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else if (num1 > num3)
+{
+    Console.WriteLine("Первое число больше третьего");
+}
+Console.WriteLine("Первое число самое большое"); !!!!!!!!!
+if (num1 < num2)
+{
+    Console.WriteLine("Первое число меньше второго"); !!!!!!!
+}
+else if (num2 > num3)
+{
+    Console.WriteLine("Второе число больше третьего");
+}
+Console.WriteLine("Второе число самое большое"); !!!!!!!!!!
+if (num2 < num3)
+{
+    Console.WriteLine("Второе число меньше третьего"); !!!!!!!
+}
+Console.WriteLine("Третьте число самое большое"); !!!!!!
+Console.WriteLine();
