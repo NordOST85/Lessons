6d48847a93911cf959dd81f39486a8b64c903788
taran <beer.taran@mail.ru>
2022-11-01 20:05:06 +0300

    save_new_task

---
+++ Home_work1/Home_work1.3/Program3.cs
@@ -0,0 +1,11 @@
+﻿int a = 1;
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine( ));
+for (int i = a; i <= n; i++)
+{
+if (i % 2 == 0)
+{
+Console.Write(i + " ");
+}
+}
+Console.ReadKey();
