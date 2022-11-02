c0931ec2ee0582b9f462130db5aeaeea866f31aa
taran <beer.taran@mail.ru>
2022-11-03 01:01:22 +0300

    zad2

---
+++ Home_work2/Program.cs
@@ -0,0 +1,18 @@
+﻿int x = 78;
+if(x <= 99)
+{
+    Console.WriteLine("нет третьей цифры");
+}
+while (99 < x || x < 999)
+{
+     Console.WriteLine(x%10);
+     if (999 < x || x < 9999)
+     {
+        x = x / 10;
+        Console.WriteLine(x / 10 );
+     }
+     else
+     {
+        Console.WriteLine("число не работает, введи другое");
+     }
+}
