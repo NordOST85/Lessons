7bf40ea759988f4315a5c653240be98f92a3c11c
taran <beer.taran@mail.ru>
2022-11-03 23:07:35 +0300

    za3

--- Home_work2/Home_work2.2/Program3.cs
+++ Home_work2/Home_work2.2/Program3.cs
@@ -1,5 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введи число от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day > 0 && day <= 5) 
+{
+    Console.WriteLine("Будний день "+ day);
+}
+if (day == 6 || day == 7) 
+{
+    Console.WriteLine("Выходной день "+ day);
+}
