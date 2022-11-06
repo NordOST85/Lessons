d18bbf724395f8acdae9494753239cd7f699f978
taran <beer.taran@mail.ru>
2022-11-06 15:23:15 +0300

    HW_4.11

--- Home_work4/Home_work4.1/Program.cs
+++ Home_work4/Home_work4.1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int Exponentiation(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}
+
+  Console.Write("Введи число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введи число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Ответ: " + exponentiation);
