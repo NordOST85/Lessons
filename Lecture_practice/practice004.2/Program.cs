d6dc71a4c6f913793c70a2254410bf8c076669b2
taran <beer.taran@mail.ru>
2022-11-06 11:16:18 +0300

    PL4.2

---
+++ Lecture_practice/practice004.2/Program.cs
@@ -0,0 +1,17 @@
+﻿//int Factorial (int n)
+//{
+//    if (n == 1) return 1;
+//    else return n * Factorial(n-1);
+//}
+//Console.WriteLine(Factorial(5));
+
+int Factorial (int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+Console.WriteLine(Factorial(5));
