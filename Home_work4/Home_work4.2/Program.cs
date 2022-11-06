5ade1741b3c471d3aea64c53e9820f31cc587165
taran <beer.taran@mail.ru>
2022-11-06 12:56:27 +0300

    HW_4.2

---
+++ Home_work4/Home_work4.2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введи число: ");
+int x = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (x > 0)
+            {
+                sum = sum + x % 10;
+                x = x /10 ;
+            }
+ Console.WriteLine(sum);
